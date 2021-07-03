b2071f8d865ddd07103b9b45765c61ec42153c1a
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-07-03 17:10:47 -0400

    expiramental

---
+++ core.py
@@ -0,0 +1,8 @@
+print("Please make new file with your address, custom, if you have not!")
+print("Make transaction, first import the address file of the other user: ")
+my_file = open("Inventory.txt", "w")
+
+content = my_file.read()
+
+
+my_file.close()
